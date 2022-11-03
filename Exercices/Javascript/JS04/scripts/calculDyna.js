ad3d5a431068f22af79a10ea0067156efddef77e
Florent <florent.delaliaux@gmail.com>
2022-11-03 13:23:36 +0100

    JS4_OKpour2

--- Exercices/Javascript/JS04/scripts/calculDyna.js
+++ Exercices/Javascript/JS04/scripts/calculDyna.js
@@ -1,26 +1,24 @@
-/*var changeQte1=document.getElementById("Qte1");
-var changeQte2=document.getElementById("Qte2");
-var changePrixUnit1=document.getElementById("PrixUnit1");
-var changePrixUnit2=document.getElementById("PrixUnit2");
-
-function Check(param1, param2) {
-    var QteActu="Qte".param2;
-   if(!isNaN(param1.value))
-   {
-
-   }
-}*/
-
 function modifTotal(event){
     sortieInput=event.target;
-    if(!isNaN(sortieInput.value)){
-        valeurActuelle=sortieInput.value;
-        if(sortieInput.getAttribute("data-input")=="Qte1"||sortieInput.getAttribute("data-input")=="PrixUnit1"){
-            valeurActuelle=
+    if(isNaN(sortieInput.value)){
+        if(sortieInput.getAttribute("data-input").match("Qte")){
+            sortieInput.value=1;
+        }else{
+            sortieInput.value=0;
         }
-    }else{
-        sortieInput.value=0;
     }
+    var ligne=sortieInput.getAttribute("data-input")[sortieInput.getAttribute("data-input").length-1];
+    qte="[data-input=Qte"+ligne+"]";
+    prixUnit="[data-input=PrixUnit"+ligne+"]";
+    prixTotal="[data-input=PrixTotal"+ligne+"]";
+    document.querySelector(prixTotal).value=document.querySelector(qte).value*document.querySelector(prixUnit).value;
+    /*
+    if(sortieInput.getAttribute("data-input").match("1")){
+        document.querySelector("[data-input=PrixTotal1]").value=document.querySelector("[data-input=Qte1]").value*document.querySelector("[data-input=PrixUnit1]").value;
+    }else{
+        document.querySelector("[data-input=PrixTotal2]").value=document.querySelector("[data-input=Qte2]").value*document.querySelector("[data-input=PrixUnit2]").value;
+    }*/
+    document.querySelector("[data-input=Total]").value=parseFloat(document.querySelector("[data-input=PrixTotal1]").value)+parseFloat(document.querySelector("[data-input=PrixTotal2]").value);
 }
 
 lesInputs = document.querySelectorAll("[data-input]");
