fad5ec182428522bcc7bf96ecf6e1790263369e5
Florent <florent.delaliaux@gmail.com>
2022-11-03 14:07:39 +0100

    JS4_Multiligne

--- Exercices/Javascript/JS04/scripts/calculDyna.js
+++ Exercices/Javascript/JS04/scripts/calculDyna.js
@@ -1,27 +1,29 @@
-function modifTotal(event){
-    sortieInput=event.target;
-    if(isNaN(sortieInput.value)){
-        if(sortieInput.getAttribute("data-input").match("Qte")){
-            sortieInput.value=1;
-        }else{
-            sortieInput.value=0;
+function modifTotal(event) {
+    sortieInput = event.target;
+    if (!isNaN(sortieInput.value)) {
+        //Get line
+        if (sortieInput.getAttribute("data-quantite")) {
+            ligne = sortieInput.getAttribute("data-quantite");
+        } else {
+            ligne = sortieInput.getAttribute("data-prixUnit");
         }
+        document.querySelector("[data-prixTotal=\""+ligne+"\"]").value = document.querySelector("[data-quantite=\"" + ligne + "\"]").value * document.querySelector("[data-prixUnit=\"" + ligne + "\"]").value;
+        lesTotaux = document.querySelectorAll("[data-prixTotal]");
+        totalFin = 0;
+        lesTotaux.forEach(element => {
+            totalFin += parseFloat(element.value);
+            console.log(totalFin);
+        });
+        document.getElementById("Total").value = totalFin;
     }
-    var ligne=sortieInput.getAttribute("data-input")[sortieInput.getAttribute("data-input").length-1];
-    qte="[data-input=Qte"+ligne+"]";
-    prixUnit="[data-input=PrixUnit"+ligne+"]";
-    prixTotal="[data-input=PrixTotal"+ligne+"]";
-    document.querySelector(prixTotal).value=document.querySelector(qte).value*document.querySelector(prixUnit).value;
-    /*
-    if(sortieInput.getAttribute("data-input").match("1")){
-        document.querySelector("[data-input=PrixTotal1]").value=document.querySelector("[data-input=Qte1]").value*document.querySelector("[data-input=PrixUnit1]").value;
-    }else{
-        document.querySelector("[data-input=PrixTotal2]").value=document.querySelector("[data-input=Qte2]").value*document.querySelector("[data-input=PrixUnit2]").value;
-    }*/
-    document.querySelector("[data-input=Total]").value=parseFloat(document.querySelector("[data-input=PrixTotal1]").value)+parseFloat(document.querySelector("[data-input=PrixTotal2]").value);
 }
 
-lesInputs = document.querySelectorAll("[data-input]");
-lesInputs.forEach(element => {
+lesQtes = document.querySelectorAll("[data-quantite]");
+lesQtes.forEach(element => {
+    element.addEventListener("blur", modifTotal);
+});
+
+lesPrixUnit = document.querySelectorAll("[data-prixUnit]");
+lesPrixUnit.forEach(element => {
     element.addEventListener("blur", modifTotal);
 });
