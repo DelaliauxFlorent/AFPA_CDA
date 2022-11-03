59c66fe49e9b26361dba63118b39d43cb15e3797
Florent <florent.delaliaux@gmail.com>
2022-11-03 17:18:10 +0100

    03/11/2022 17:18:10,10

--- Exercices/Javascript/JS07/scripts/scripts.js
+++ Exercices/Javascript/JS07/scripts/scripts.js
@@ -1,4 +1,12 @@
-bouton=document.getElementsByTagName(button)[0];
+bouton=document.getElementsByTagName("button")[0];
 bouton.addEventListener("click", ajouterDessert);
 
-function
+function ajouterDessert(){
+    newDessert = prompt("Quel dessert ajouter?");
+    listeDesserts=document.getElementById("listeDesserts");
+    temp = document.getElementsByTagName("template")[0];
+    const element = temp.content.cloneNode(true);
+    element.textcontent=newDessert;
+    //console.info(element);
+    listeDesserts.appendChild(element);
+}
