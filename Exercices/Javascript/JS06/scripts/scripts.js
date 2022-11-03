067c3414750472f75ae87a75aea0cedee1f6675a
Neodart <neodart@gmail.com>
2022-11-03 19:57:54 +0100

    JS06_ModifPassageParClasses

--- Exercices/Javascript/JS06/scripts/scripts.js
+++ Exercices/Javascript/JS06/scripts/scripts.js
@@ -1,16 +1,21 @@
-listeCouleursParag=['blue', 'black'];
-listeCouleursTitre=['yellow', 'green', 'black'];
+listeCouleursParag = ['blue', 'black'];
+listeCouleursTitre = ['yellow', 'green', 'black'];
 
-lesParagraphes=document.querySelectorAll("p");
+lesParagraphes = document.querySelectorAll("p");
 
 lesParagraphes.forEach(para => {
     para.addEventListener("click", changeColorP);
 });
-lesTitres=document.querySelectorAll("h1, h2, h3");
+lesTitres = document.querySelectorAll("h1, h2, h3");
 lesTitres.forEach(title => {
     title.addEventListener("click", changeColorT);
 });
 
+var iteration=1;
+
+////////////////////////////////////////////////////////////
+// Alternance couleur en changeant style
+/*
 function changeColorP(event) {
     paragraphe=event.target;
     if(paragraphe.style.color==listeCouleursParag[0]){
@@ -37,4 +42,28 @@ function changeColorT(event) {
         }       
         
     });
+}
+*/
+
+////////////////////////////////////////////////////////////
+// Alternance couleur en changeant les classes
+
+function changeColorP(event) {
+    paragraphe = event.target;
+    if (paragraphe.classList.contains("para2")) {
+        paragraphe.classList.remove("para2");
+    } else {
+        paragraphe.classList.add("para2");
+    }
+}
+
+function changeColorT(event) {
+    titre=event.target;
+    tousTitreNiveau=document.querySelectorAll(titre.localName);
+    tousTitreNiveau.forEach(title => {
+        title.classList.toggle("titleA", iteration%3==0);
+        title.classList.toggle("titleB", iteration%3==1);
+        title.classList.toggle("titleC", iteration%3==2);      
+    });    
+    iteration++;  
 }
