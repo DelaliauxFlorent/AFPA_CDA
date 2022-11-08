03cf1df0c59bc4e98323d6716b2b42192d645f70
Neodart <neodart@gmail.com>
2022-11-08 20:51:16 +0100

    JS11_Formulaire_MiseEnPage&MsgErreurs

--- Exercices/Javascript/JS11 - Formulaires/V2/scripts/scripts.js
+++ Exercices/Javascript/JS11 - Formulaires/V2/scripts/scripts.js
@@ -1,73 +1,90 @@
-
+/////////////////////////////////////////
+// Fait la liste de tous les inputs sauf le bouton "submit"
+// => ajout d'un eventListener sur chaque pour vérifier les saisies
 listInputs = document.querySelectorAll('[data-type]');
 listInputs.forEach(element => {
     element.addEventListener("blur", verifInput);
 });
 
+
+/**
+ * Fonction vérifiant que ce qui est entré dans les Inputs correspond bien aux patterns définis
+ * @param {*} event 
+ */
 function verifInput(event) {
     element = event.target;
-    typeElem = element.getAttribute("data-type");
+    typeElem = element.getAttribute("data-type");       // Récupère le type d'Input
     erreur = false;
 
-    if (typeElem == "cdPost") {
+    if (typeElem == "cdPost") {                         // codage du pattern pour le code postal en dur du fait de l'absence d'un attribut pattern pour les inputs de type "number"
         regexString = "\\d{5}";
     }
     else {
-        regexString = element.getAttribute("pattern")
+        regexString = element.getAttribute("pattern")   // dans les autres cas, on utilise le pattern déjà fournit
     }
-    regexActu = new RegExp(`\^${regexString}\$`, "u");
+    regexActu = new RegExp(`\^${regexString}\$`, "u");  // on crée le regex à partir des patterns
 
-    console.info(regexActu);
+    //console.info(regexActu);
 
-    if (!element.value.match(regexActu)) {
-        element.value = "";
-        gestionErreur(element, typeElem);
+    if (!element.value.match(regexActu)) {              // s'il n'y a pas de correspondance
+        element.value = "";                             // on réinitialise l'input
+        gestionErreur(typeElem);                        // et on affiche le message d'erreur correspondant
         erreur = true;
     }
     else {
+        // si l'entrée est bonne, on ne fait que supprimer le message d'erreur correspondant au cas où il existe
         erreur = false;
+        vidageMessageErreur = document.querySelector('[data-message="'+typeElem+'"]');
+        vidageMessageErreur.innerHTML="";        
+        resetInputClass=document.querySelector('[data-type="'+typeElem+'"]');
+        resetInputClass.classList.remove("erreur");
     }
 
-    verifSubmit();
+    verifSubmit(erreur);
 }
 
-function gestionErreur(params, typeInput) {
-    messageErreur = "Erreur de saisie!\n"
+function gestionErreur(typeInput) {
+    messageFlash=document.querySelector('[data-message="'+typeInput+'"]');
+    inputFlash=document.querySelector('[data-type="'+typeInput+'"]');
+
+    // En fonction du type de donnée sur laquelle l'erreur a été faite
+    // on affiche le message correspondant en dessous de l'input conserné
 
     switch (typeInput) {
         case "nom":
-            messageErreur += "Un nom peut avoir des espaces ou des tirets mais ne dois sinon comporter que des lettres.";
+            messageFlash.innerHTML= "Un nom peut avoir des espaces ou des tirets mais ne dois sinon comporter que des lettres.";
             break;
         case "telephone":
-            messageErreur += "Un numéro de téléphone doit être une suite de chiffres de la forme 0X XX XX XX XX ou +33X XX XX XX XX (les espaces pouvant être remplacés par des points ou même enlevés).";
+            messageFlash.innerHTML= "Un numéro de téléphone doit être une suite de chiffres de la forme 0X&nbsp;XX&nbsp;XX&nbsp;XX&nbsp;XX ou +33X&nbsp;XX&nbsp;XX&nbsp;XX&nbsp;XX (les espaces pouvant être remplacés par des points ou même enlevés).";
             break;
-        case "cdPostal":
-            messageErreur += "Un code postal doit être une suite de 5 chiffres.";
+        case "cdPost":
+            messageFlash.innerHTML= "Un code postal doit être une suite de 5 chiffres.";
             break;
         case "mail":
-            messageErreur += "Une adresse mail est formé d'une suite de caractères (séparé ou non par un point), d'un arobase puis d'une autre suite de caractère et se termine par un '.xxx' (où xxx est une suite de 2 à 5 caractères).";
+            messageFlash.innerHTML= "Une adresse mail est formé d'une suite de caractères (séparé ou non par un point), d'un arobase puis d'une autre suite de caractère et se termine par un '.xxx' (où xxx est une suite de 2 à 5 caractères).";
             break;
         case "mdp":
-            messageErreur += "Un mot de passe doit mesurer au moins 8 caractères et comporter au moins une lettre minuscule, une lettre majuscule, un chiffre et un caractère spécial (+-*/%...).";
+            messageFlash.innerHTML= "Un mot de passe doit mesurer au moins 8 caractères et comporter au moins une lettre minuscule, une lettre majuscule, un chiffre et un caractère spécial (+-*/%...).";
             break;
         default:
             break;
     }
-    alert(messageErreur);
-
-    ///////////////////////////////////////////////////
-    // Look up how to make it work
-    console.info(params);
-    params.focus();
+    
+    // et on change l'apparence de l'input en question pour attirer l'oeil de l'utilisateur
+    inputFlash.classList.add("erreur");
 }
 
-function verifSubmit() {
+function verifSubmit(erreurExt) {
     peutEnvoyer = true;
     listInputs.forEach(element => {
         peutEnvoyer &= element.value != "";
     });
-    if (peutEnvoyer && !erreur) {
-        document.getElementById("envoyer").disabled = false;
+
+    // si tous les champs sont rempli (possibilité de check seulement ceux avec attribut "required")
+    // et qu'aucune erreur n'est détecter lors des saisies
+
+    if (peutEnvoyer && !erreurExt) {
+        document.getElementById("envoyer").disabled = false;        // on réactive le bouton submit
     }
     else {
         document.getElementById("envoyer").disabled = true;
