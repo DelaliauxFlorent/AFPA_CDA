7d5c7e05355ec2fd4d6799342f7b29110247e938
Neodart <neodart@gmail.com>
2022-11-11 17:06:53 +0100

    Gerenation_auto_Formulaire_Fini

--- Exercices/Generateur_formulaire/index.php
+++ Exercices/Generateur_formulaire/index.php
@@ -1,5 +1,9 @@
 <?php
+/////////////////////////////////////////////////////////////////////////
+// Setting CONSTANTs to facilitate dealing with non-associative arrays
+/////////////////////////////////////////////////////////////////////////
 
+// For the array dealing with all the fields for each INPUT
 const LABEL = 0;
 const ID = 1;
 const VALUE = 2;
@@ -9,8 +13,60 @@ const REQUIRED = 5;
 const HIDDEN = 6;
 const DISABLED = 7;
 
-////////////////////////////////////////////////////////////
-// Partie haute du HTML
+// For the patterns and corrresponding message
+const REGEX = 0;
+const MESSAGE = 1;
+
+/////////////////////////////////////////////////////////////////////////
+// Setting an associative array where each KEY correspond to a pair
+// "Regex pattern" => corresponding message
+/////////////////////////////////////////////////////////////////////////
+
+$patterns = [
+    "Alpha" => ["^[a-zA-Z]{2,}$", "Entrer deux lettres minimum"],
+    "AlphaMin" => ["[a-z]", "Entrer une lettre minuscule"],
+    "AlphaMaj" => ["[A-Z]", "Entrer une lettre majuscule"],
+    "Num" => ["[0-9]", "Entrer un chiffre"],
+    "Spec" => ["[\W_]", "Entrer un caractère spécial (+-*/%...)"],
+    "Telephone" => ["^[0]\d(\.| )?\d{2}(\.| )?\d{2}(\.| )?\d{2}(\.| )?\d{2}$", "10 chiffres, espaces et points autorisés."],
+    "Mail" => ["^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,5})+$", "Une adresse de la forme 'Abc@example.com'."],
+    "Min1Min" => ["(?=.*[a-z])", "1 minuscule"],
+    "Min1Maj" => ["(?=.*[A-Z])", "1 majuscule"],
+    "Min1Chi" => ["(?=.*[0-9])", "1 chiffre"],
+    "Min1Spe" => ["(?=.*[\W_])", "1 caractère spécial (+-*/%...)"],
+    "Min8Car" => [".{8,}", "8 caractères minimum"]
+    // "PassW"=> ["(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\W_]).{8,}",]
+];
+
+/////////////////////////////////////////////////////////////////////////
+// Setting the particular case of the password
+/////////////////////////////////////////////////////////////////////////
+
+$patternPW = [$patterns["Min1Min"], $patterns["Min1Maj"], $patterns["Min1Chi"], $patterns["Min1Spe"], $patterns["Min8Car"]];
+
+/////////////////////////////////////////////////////////////////////////
+// Creating the array with all the inputs we want in the form
+/////////////////////////////////////////////////////////////////////////
+
+// For each element, the fields correspond to => "label", "id", "value", "type", "pattern", "required", "hidden", "disabled"
+// ("value" is always empty here but the field would be needed if we took the infos from a DB)
+// pattern => either a predefinied regex or, in case of type="number", a string with the "min" and "max" values separated by a ","
+$listeDonneesFormulaire = [
+    "nom" =>        ["Nom",                 "nom",          "",     "text",         $patterns["Alpha"],     true,       false,      false],
+    "prenom" =>     ["Prénom",              "prenom",       "",     "text",         $patterns["Alpha"],     false,      false,      false],
+    "age" =>        ["Âge",                 "age",          "",     "number",       "0,100",                false,      false,      false],
+    "telephone" =>  ["Numéro de téléphone", "telephone",    "",     "text",         $patterns["Telephone"], false,      false,      false],
+    "cdPostal" =>   ["Code Postal",         "cdPostal",     "",     "number",       "1000,99999",           false,      false,      false],
+    "mail" =>       ["Mail",                "mail",         "",     "email",        $patterns["Mail"],      true,       false,      false],
+    "password" =>   ["Mot de passe",        "mdp",          "",     "password",     $patternPW,             true,       false,      false],
+    "confirmer" =>  ["Confirmation",        "confirmPW",    "",     "password",     $patternPW,             true,       false,      true]
+];
+//Repeat for clarity:"label",               "id",           "value","type",         "pattern",              "required", "hidden",   "disabled
+
+/////////////////////////////////////////////////////////////////////////
+// Start of the HTML page (<head>, Title and opening the <form>)
+/////////////////////////////////////////////////////////////////////////
+
 echo '
 <!DOCTYPE html>
 <html lang="en">
@@ -22,143 +78,108 @@ echo '
     <link rel="stylesheet" type="text/css" href="./styles/styles.css">
     <link rel="icon" type="image/x-icon" href="./img/favicon.ico">
     <script src="https://kit.fontawesome.com/ce4feb7268.js" crossorigin="anonymous"></script>
-    <title>Générateur de Formulaires</title>
+    <title>Génération de formulaires</title>
 </head>
 
 <body>
-    <h1>Formulaire</h1>
+    <h1>Génération de formulaires</h1>
         <form action="#" method="post">';
 
-// Fin de haute du HTML
-////////////////////////////////////////////////////////////
-
-// Gestion des différents patterns possible
-$patterns = [
-    "Alpha" => ["^[a-zA-Z]{2,}$", "Entrer deux lettres minimum"],
-    "AlphaMin" => ["[a-z]", "Entrer une lettre minuscule"],
-    "AlphaMaj" => ["[A-Z]", "Entrer une lettre majuscule"],
-    "Num" => ["[0-9]", "Entrer un chiffre"],
-    "Spec" => ["[\W_]", "Entrer un caractère spécial (+-*/%...)"],
-    "Telephone" => ["^[0]\d(\.| )?\d{2}(\.| )?\d{2}(\.| )?\d{2}(\.| )?\d{2}$", "10 chiffres, espaces et points autorisés."],
-    "Mail" => ["^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,5})+$", "Une adresse de la forme 'Abc@example.com'."],
-    "Min1Min" => ["(?=.*[a-z])", "Au moins 1 minuscule, "],
-    "Min1Maj" => ["(?=.*[A-Z])", "1 majuscule, "],
-    "Min1Chi" => ["(?=.*[0-9])", "1 chiffre, "],
-    "Min1Spe" => ["(?=.*[\W_])", "1 caractère spécial (+-*/%...) et "],
-    "Min8Car" => [".{8,}", "au moins 8 caractères au total"]
-    // "PassW"=> ["(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\W_]).{8,}",]
-];
-
-$patternPW = [$patterns["Min1Min"], $patterns["Min1Maj"], $patterns["Min1Chi"], $patterns["Min1Spe"], $patterns["Min8Car"]];
-// Pour chaque élément, ordres => "label", "id", "value", "type", "pattern", "required", "hidden", "disabled"
-$listeDonneesFormulaire = [
-    "nom" => ["Votre nom : ", "nom", "", "text", $patterns["Alpha"], true, false, false],
-    "prenom" => ["Votre prenom : ", "prenom", "", "text", $patterns["Alpha"], false, false, false],
-    "age" => ["Votre âge : ", "age", "", "number", "0,100", false, false, false],
-    "telephone" => ["Numéro de téléphone : ", "telephone", "", "text", $patterns["Telephone"], false, false, false],
-    "cdPostal" => ["votre Code Postal : ", "cdPostal", "", "number", "1000,99999", false, false, false],
-    "mail" => ["Votre eMail : ", "mail", "", "email", $patterns["Alpha"], true, false, false],
-    "password" => ["Mot de passe : ", "mdp", "", "password", $patternPW, true, false, false],
-    "confirmer" => ["Confirmation : ", "confirmPW", "", "password", $patternPW, true, false, true]
-];
-
-////////////////////////////////////////////////////////////
-// Boucle de création du formulaire
+/////////////////////////////////////////////////////////////////////////
+// Loop for each of the desired INPUTs
+/////////////////////////////////////////////////////////////////////////
 
 foreach ($listeDonneesFormulaire as $ligne => $champ) {
+    // each input on its own line, start with the <label>, then the <input> with every fields that will be the same no matter what 
     echo '
-    <div class="ligne">
-        <label for="' . $champ[ID] . '">' . $champ[LABEL] . '</label>
-        <input class="checkValidity" type="' . $champ[TYPE] . '" id="' . $champ[ID] . '" name="' . $champ[ID] . '" ' . (($champ[REQUIRED]) ? 'required ' : '') . (($champ[HIDDEN]) ? 'hidden ' : '') . (($champ[DISABLED]) ? 'disabled ' : '');
-    if ($champ[TYPE] == "number") {
-        $minMax = explode(",", $champ[PATTERN]);
-        $titleNum="Un nombre compris entre $minMax[0] et $minMax[1]";
-        echo ' min=' . $minMax[0] . ' max=' . $minMax[1] . ' title="'.$titleNum.'" />';
-    } elseif ($champ[TYPE] == "password") {
-        $patPW = "";
-        $titlePW = "";
-        foreach ($champ[PATTERN] as $value) {
-            $patPW .= $value[0];
-            $titlePW .= $value[1];
-        }
-        echo ' pattern="'.$patPW.'" title="'.$titlePW.'" />';
-    } else {
-        echo ' pattern="'.$champ[PATTERN][0].'" title="'.$champ[PATTERN][1].'" />';
-    }
-    
-    if($champ[TYPE] == "number"){
-                echo '
-                <div class="mini"></div>
-                <div class="info picto">
-                    <i class="far fa-question-circle fa-2x infobulle"></i>
-                    <div class="texteInfoBulle">'.$titleNum.'</div>
-                </div>
-                <div class="check picto">
-                    <i class="far fa-check-circle fa-2x vert"></i>
-                </div>
-                <div class="message"></div>';
-            }elseif($champ[TYPE] == "password"){
-                echo '
-                <div class="mini info">
-                    <div class="oeil">
-                        <i class="fas fa-eye"></i>
-                    </div>
-                </div>
-                <div class="info picto">
-                    <i class="far fa-question-circle fa-2x infobulle"></i>
-                    <div class="texteInfoBulle">'.$titlePW.'</div>
-                    <div class="aideMdp">
-                        <div>Liste des critères à respecter</div>
-                        <div>
-                            <div class="mini"><i class="far fa-times-circle rouge"></i>
-                            </div>
-                            <div class="gauche">8 caractères minimum</div>
-                        </div>
-                        <div>
-                            <div class="mini"><i class="far fa-times-circle rouge"></i>
-                            </div>
-                            <div class="gauche">majuscule(s)</div>
-                        </div>
-                        <div>
-                            <div class="mini"><i class="far fa-times-circle rouge"></i>
-                            </div>
-                            <div class="gauche">minuscule(s)</div>
-                        </div>
-                        <div>
-                            <div class="mini"><i class="far fa-times-circle rouge"></i>
-                            </div>
-                            <div class="gauche">nombre(s)</div>
-                        </div>
-                        <div>
-                            <div class="mini"><i class="far fa-times-circle rouge"></i>
-                            </div>
-                            <div class="gauche">caractères spéciaux</div>
+            <div class="ligne">
+                <label for="' . $champ[ID] . '">' . $champ[LABEL] . '</label>
+                <input class="checkValidity" type="' . $champ[TYPE] . '" id="' . $champ[ID] . '" name="' . $champ[ID] . '" value="'.$champ[VALUE].'" ' . (($champ[REQUIRED]) ? 'required ' : '') . (($champ[HIDDEN]) ? 'hidden ' : '') . (($champ[DISABLED]) ? 'disabled ' : '');
+
+                // Depending on the "type", the "pattern" and "title" fields will be differents
+                if ($champ[TYPE] == "number") {
+                    $minMax = explode(",", $champ[PATTERN]);                                                // if a number, get min and max value
+                    $titleNum = "Un nombre compris entre $minMax[0] et $minMax[1]";                         // generate the title and
+                    echo ' min=' . $minMax[0] . ' max=' . $minMax[1] . ' title="' . $titleNum . '" />';     // add those 3 fields to the current input
+                } elseif ($champ[TYPE] == "password") {
+                    // if a password, initialize a pattern and title variable
+                    $patPW = "";
+                    $titlePW = "";
+                    foreach ($champ[PATTERN] as $value) {
+                        // fill those with each of the given elements
+                        $patPW .= $value[REGEX];
+                        $titlePW .= $value[MESSAGE].", ";
+                    }
+                    $titlePW=substr($titlePW, 0, -2);
+                    // and add the fields with the correct values
+                    echo ' pattern="' . $patPW . '" title="' . $titlePW . '" />';
+                } else {
+                    // in any other case, fill those fields directly
+                    echo ' pattern="' . $champ[PATTERN][REGEX] . '" title="' . $champ[PATTERN][1] . '" />';
+                }
+                // We are done with the <input>, now the various messages specific to this particular one
+
+        if ($champ[TYPE] == "password") {
+            // If we are dealing with a password, we add the possibility for the user to clearly display
+            // the field for a short while (done in JavaScript) => Eye icon to the right of the input
+            // Then we add a way to remind the user, again, what the requirement are for the validity of the field
+            echo '
+                    <div class="mini info">
+                        <div class="oeil">
+                            <i class="fas fa-eye"></i>
                         </div>
                     </div>
-                </div>
-                <div class="check picto">
-                    <i class="far fa-check-circle fa-2x vert"></i>
-                </div>
-                <div class="message"></div>';
-            }else{
+                    <div class="info picto">
+                        <i class="far fa-question-circle fa-2x infobulle"></i>
+                        <div class="texteInfoBulle">' . $titlePW . '</div>';
+            if ($champ[ID] != "confirmPW") {
+                // In the case of the 1st itération of the password, add a tool to show what conditions
+                // the current value of the input fulfil
                 echo '
-                <div class="mini"></div>
-                <div class="info picto">
-                    <i class="far fa-question-circle fa-2x infobulle"></i>
-                    <div class="texteInfoBulle">'.$champ[PATTERN][1].'</div>
-                </div>
-                <div class="check picto">
-                    <i class="far fa-check-circle fa-2x vert"></i>
-                </div>
-                <div class="message"></div>';
+                            <div class="aideMdp">
+                            <div>Liste des critères à respecter</div>';
+                    foreach ($champ[PATTERN] as $value) {
+                        echo'
+                            <div>
+                                <div class="mini"><i class="far fa-times-circle rouge"></i>
+                                </div>
+                                <div class="gauche">'.$value[MESSAGE].'</div>
+                            </div>';
+                    }
+                echo '
+                    </div>';   
             }
+            // Add a green check or a red cross displaying the validity of the input
+            // as well as the text indicating the same
+        echo '
+                </div>
+                <div class="check picto">
+                    <i class="far fa-check-circle fa-2x vert"></i>
+                </div>
+                <div class="message"></div>';
+    } else {
+        // We do the equivalent for all other cases
+        echo '
+                <div class="mini"></div>
+                <div class="info picto">
+                    <i class="far fa-question-circle fa-2x infobulle"></i>
+                    <div class="texteInfoBulle">' .(($champ[TYPE] == "number")?$titleNum:$champ[PATTERN][MESSAGE]). '</div>
+                </div>
+                <div class="check picto">
+                    <i class="far fa-check-circle fa-2x vert"></i>
+                </div>
+                <div class="message"></div>';
+    }
     echo '
     
     </div>';
 }
 
-////////////////////////////////////////////////////////////
-// Partie basse du HTML
+/////////////////////////////////////////////////////////////////////////
+// End of the HTML page (adding the submit button, closing the <form>,
+// linking the script and closing everything else)
+/////////////////////////////////////////////////////////////////////////
+
 echo '
         <div class="ligne"><input type="submit"></div>
     </form>
--- Exercices/Generateur_formulaire/scripts/scripts.js
+++ Exercices/Generateur_formulaire/scripts/scripts.js
@@ -6,20 +6,21 @@ var submit = document.querySelector("[type=submit]");
 var mdp = document.getElementById("mdp");
 var confirmation = document.getElementById("confirmPW");
 
-
 //Ajoute la vérification de validité au changement de chaque champ
 for (let i = 0; i < inputs.length; i++) {
     inputs[i].addEventListener("input", function (event) {
         updateValidity(event.target);
     });
 }
+
 // affichage de l'aide à la saisie du mot de passe 
 // c'est la 2eme fois que l'on pose un listener sur input de mot de passe, les 2 fonctions vont s'executer l'une derrière l'autre 
 mdp.addEventListener("input", function (event) {
     let aideMdp = document.getElementsByClassName("aideMdp")[0];
-    aideMdp.style.display = "flex";
+    aideMdp.classList.add("displayFlex");
+    aideMdp.classList.remove("noDisplay");
     let lesImages = aideMdp.getElementsByTagName("i");
-    let lesCheck = ["(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\\W_]).{8,}", "(?=.*[A-Z])", "(?=.*[a-z])", "(?=.*[0-9])", "(?=.*[\\W_])"];
+    let lesCheck = ["(?=.*[a-z])", "(?=.*[A-Z])", "(?=.*[0-9])", "(?=.*[\\W_])", "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\\W_]).{8,}"];
     for (let i = 0; i < lesCheck.length; i++) {
         if (RegExp(lesCheck[i]).test(mdp.value)) {
             //la condition est vérifiée, on met la coche verte correspondente
@@ -29,10 +30,14 @@ mdp.addEventListener("input", function (event) {
         }
     }
 })
+
 //suppression de l'aide mot de passe quand on quitte le champ
 mdp.addEventListener("blur", function (event) {
-    document.getElementsByClassName("aideMdp")[0].style.display = "none";
+    let aideMdp = document.getElementsByClassName("aideMdp")[0];
+    aideMdp.classList.remove("displayFlex");
+    aideMdp.classList.add("noDisplay");
 })
+
 //gestion particulière de la confirmation de mot de passe
 confirmation.addEventListener("input", function (event) {
     if (confirmation.value == mdp.value) {
@@ -41,6 +46,7 @@ confirmation.addEventListener("input", function (event) {
         impactValidity(confirmation, false);
     }
 })
+
 //empecher le copier dans la zone mdp et confirm
 mdp.addEventListener("contextmenu", annule);
 confirmation.addEventListener("contextmenu", annule);
@@ -48,13 +54,15 @@ confirmation.addEventListener("contextmenu", annule);
 confirmation.addEventListener("paste", annule);
 
 //gestion de l'oeil dans le mot de passe
-var oeil = document.getElementsByClassName("oeil")[0];
-// on affiche un petit oeil qui permet de voir de mot de passe 
-oeil.addEventListener("mousedown", function () {
-    affichePassWord(true);
-});
-oeil.addEventListener("mouseup", function () {
-    affichePassWord(false);
+var yeux=document.querySelectorAll(".oeil");
+yeux.forEach(oeil => {
+    // on affiche un petit oeil qui permet de voir de mot de passe 
+    oeil.addEventListener("mousedown", function (event) {
+        affichePassWord(true,event.target.parentNode.parentNode.previousElementSibling.id);
+    });
+    oeil.addEventListener("mouseup", function (event) {
+        affichePassWord(false,event.target.parentNode.parentNode.previousElementSibling.id);
+    });
 });
 
 /**
@@ -65,13 +73,19 @@ infobulles = document.getElementsByClassName("infobulle");
 for (let i = 0; i < infobulles.length; i++) {
     infobulles[i].addEventListener("mouseover", function (e) {
         var parent = e.target.parentNode.parentNode;
-        // var texte = (parent.getElementsByTagName("input")[0]).title;
         var texteInfoBulle = parent.getElementsByClassName("texteInfoBulle")[0];
+
+        // Géré par le PHP ici
+
+        // var texte = (parent.getElementsByTagName("input")[0]).title;
         // texteInfoBulle.textContent = texte;
-        texteInfoBulle.style.display = "flex";
+
+        texteInfoBulle.classList.remove("noDisplay");
+        texteInfoBulle.classList.add("displayFlex");
     });
     infobulles[i].addEventListener("mouseout", function (e) {
-        (e.target.parentNode.getElementsByClassName("texteInfoBulle")[0]).style.display = "none";
+        (e.target.parentNode.getElementsByClassName("texteInfoBulle")[0]).classList.remove("displayFlex");
+        (e.target.parentNode.getElementsByClassName("texteInfoBulle")[0]).classList.add("noDisplay");
     });
 }
 
@@ -109,31 +123,65 @@ function impactValidity(input, isValid) {
 
     var message = input.parentNode.getElementsByClassName("message")[0];
     message.classList.add("visible");
-    image = input.parentNode.getElementsByTagName("i")[1];
+    if (input.type == "password") {
+        // Si password, index 0 correspond à l'oeil
+        image = input.parentNode.getElementsByTagName("i")[1];
+    } else {
+        image = input.parentNode.getElementsByTagName("i")[0];
+    }
+
+    image.parentNode.classList.toggle("check", isValid != 0);
+    input.classList.toggle("basVert", isValid);
+    input.classList.toggle("basRouge", !isValid);
 
     switch (isValid) {
         case true:
             message.innerHTML = "Champ valide.";
-            input.style.borderBottomColor = "rgb(50,200,50)";
             image.classList = "far fa-check-circle fa-2x vert";
-            image.style.display = "block";
-            console.info(input);
-            if(input.id=="mdp"){
-                document.getElementById("confirmPW").disabled=false;
+
+            image.classList.add("displayBlock");
+            image.classList.remove("noDisplay");
+
+            image.parentNode.classList.toggle("check", !isValid);
+
+            // si l'on vient de trouver que le mot de passe est bon,
+            // on réactive l'input pour sa confirmation
+            if (input.id == "mdp") {
+                confirmation.disabled = false;
             }
             break;
         case 0:
             message.innerHTML = "Champ requis!";
-            image.style.display = "none";
+            image.classList.remove("displayBlock");
+            image.classList.add("noDisplay");
             break;
         case false:
-            message.innerHTML = "Format invalide!";
-            input.style.borderBottomColor = "rgb(200,50,50)";
-            image.classList = "far fa-times-circle fa-2x rouge";
-            image.style.display = "block";
-            if(input.id=="mdp"){
-                document.getElementById("confirmPW").disabled=true;
+            if (input.id == "confirmPW") {
+                // si on est sur la confirmation du mot de passe
+                // la seule erreur possible ici est qu'il n'est pas identique
+                message.innerHTML = "Non identique!";
+            } else {
+                message.innerHTML = "Format invalide!";
             }
+            image.classList = "far fa-times-circle fa-2x rouge";
+
+            image.classList.add("displayBlock");
+            image.classList.remove("noDisplay");
+
+            image.parentNode.classList.toggle("check", isValid);
+
+            // si l'on vient de trouver que le mot de passe n'est pas bon,
+            // on désactive et réinitialise l'input de confirmation
+            // et les messages associé
+
+            if (input.id == "mdp") {
+                confirmation.disabled = true;
+                confirmation.value="";
+                confirmation.classList.remove("basVert");
+                confirmation.parentNode.querySelectorAll("i")[1].classList="far fa-question-circle fa-2x infobulle";
+                confirmation.parentNode.querySelector(".message").classList.remove("visible");
+            }
+
             break;
         default:
             break;
@@ -162,14 +210,18 @@ function checkAllValidity() {
         submit.style.borderBottom = "4px solid #666666";
     }
 }
+
 /**
  * Change le type de l'input mot de passe
+ * que l'utilisateur veut vérifier
  * @param {boolean} flag 
  */
-function affichePassWord(flag) {
-    if (flag) mdp.type = "text";
-    else mdp.type = "password";
+function affichePassWord(flag, id) {
+    afficheMDP=document.getElementById(id);
+    if (flag) afficheMDP.type = "text";
+    else afficheMDP.type = "password";
 }
+
 /**
  * Annule l'action associé à la touche ou au clic
  * @param {*} event 
