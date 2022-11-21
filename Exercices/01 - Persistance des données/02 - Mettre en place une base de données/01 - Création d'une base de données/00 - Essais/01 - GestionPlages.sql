c3e98898fb8df9185a7b9ed9df6166fab3fbd8da
Neodart <neodart@gmail.com>
2022-11-21 17:23:15 +0100

    21/11/22 - 17h20 - ForeignKeys

--- Exercices/01 - Persistance des données/02 - Mettre en place une base de données/01 - Création d'une base de données/00 - Essais/01 - GestionPlages.sql
+++ Exercices/01 - Persistance des données/02 - Mettre en place une base de données/01 - Création d'une base de données/00 - Essais/01 - GestionPlages.sql
@@ -1,38 +1,66 @@
-CREATE DATABASE IF NOT EXISTS GestionPlages;
+DROP DATABASE IF EXISTS GestionPlages;
+
+CREATE DATABASE GestionPlages DEFAULT CHARACTER SET utf8;
+
 USE GestionPlages;
 
 DROP TABLE IF EXISTS NatureTerrains;
+
 CREATE TABLE NatureTerrains(
    idNatureTerrain INT AUTO_INCREMENT PRIMARY KEY,
-   nomNatureTerrain VARCHAR(50)  NOT NULL
+   nomNatureTerrain VARCHAR(50) NOT NULL
 );
 
 DROP TABLE IF EXISTS Departements;
+
 CREATE TABLE Departements(
    idDep VARCHAR(50) PRIMARY KEY,
    nomDep VARCHAR(50)
 );
 
 DROP TABLE IF EXISTS Villes;
+
 CREATE TABLE Villes(
    idVille INT AUTO_INCREMENT PRIMARY KEY,
-   nomVille VARCHAR(50)  NOT NULL,
+   nomVille VARCHAR(50) NOT NULL,
    codePostal SMALLINT NOT NULL,
    nbAnTouri SMALLINT NOT NULL,
-   idDep VARCHAR(50)  NOT NULL
+   idDep VARCHAR(50) NOT NULL
 );
 
 DROP TABLE IF EXISTS Plages;
+
 CREATE TABLE Plages(
    idPlage INT AUTO_INCREMENT PRIMARY KEY,
-   nomPlage VARCHAR(50)  NOT NULL,
-   longPlage DECIMAL(6,3)   NOT NULL,
+   nomPlage VARCHAR(50) NOT NULL,
+   longPlage DECIMAL(6, 3) NOT NULL,
    idVille INT NOT NULL
 );
 
-DROP TABLE IF EXISTS compose;
-CREATE TABLE compose(
+DROP TABLE IF EXISTS Compositions;
+
+CREATE TABLE Compositions(
+   idComposition INT AUTO_INCREMENT PRIMARY KEY,
    idPlage INT,
-   idNatureTerrain INT,
-   PRIMARY KEY(idPlage, idNatureTerrain)
+   idNatureTerrain INT
 );
+
+--
+-- constraintes clés étrangères
+--
+ALTER TABLE
+   Villes
+ADD
+   CONSTRAINT FK_Villes_Departements FOREIGN KEY(idDep) REFERENCES Departements(idDep);
+
+ALTER TABLE
+   Plages
+ADD
+   CONSTRAINT FK_Plages_Villes FOREIGN KEY(idVille) REFERENCES Villes(idVille);
+
+ALTER TABLE
+   Compositions
+ADD
+   CONSTRAINT FK_Compositions_Plages FOREIGN KEY(idPlage) REFERENCES Plages(idPlage),
+ADD
+   CONSTRAINT FK_Compositions_NatureTerrains FOREIGN KEY(idNatureTerrain) REFERENCES NatureTerrains(idNatureTerrain);
--- Exercices/01 - Persistance des données/02 - Mettre en place une base de données/01 - Création d'une base de données/00 - Essais/Expressions des bessoins/11 - IGPN.sql
+++ Exercices/01 - Persistance des données/02 - Mettre en place une base de données/01 - Création d'une base de données/00 - Essais/Expressions des bessoins/11 - IGPN.sql
@@ -1,71 +1,140 @@
-CREATE DATABASE IF NOT EXISTS boeufs_carottes;
+DROP DATABASE IF EXISTS boeufs_carottes;
+
+CREATE DATABASE boeufs_carottes DEFAULT CHARACTER SET utf8;
+
 USE boeufs_carottes;
 
+--
+-- Table Blames
+--
+DROP TABLE IF EXISTS Blames;
+
 CREATE TABLE Blames(
-   idBlame COUNTER,
-   nomBlame VARCHAR(50),
-   PRIMARY KEY(idBlame)
-);
+    idBlame INT AUTO_INCREMENT PRIMARY KEY,
+    nomBlame VARCHAR(50)
+) ENGINE = InnoB;
+
+--
+-- Table Grades
+--
+DROP TABLE IF EXISTS Grades;
 
 CREATE TABLE Grades(
-   idGrade COUNTER,
-   nomGrade VARCHAR(50),
-   PRIMARY KEY(idGrade)
-);
+    idGrade INT AUTO_INCREMENT PRIMARY KEY,
+    nomGrade VARCHAR(50)
+) ENGINE = InnoB;
+
+--
+-- Table Villes
+--
+DROP TABLE IF EXISTS Villes;
 
 CREATE TABLE Villes(
-   idVille COUNTER,
-   nomVille VARCHAR(50),
-   PRIMARY KEY(idVille)
-);
+    idVille INT AUTO_INCREMENT PRIMARY KEY,
+    nomVille VARCHAR(50)
+) ENGINE = InnoB;
+
+--
+-- Table Commissariats
+--
+DROP TABLE IF EXISTS Commissariats;
 
 CREATE TABLE Commissariats(
-   idCommissariat COUNTER,
-   nomCommissariat VARCHAR(50),
-   PRIMARY KEY(idCommissariat)
-);
+    idCommissariat INT AUTO_INCREMENT PRIMARY KEY,
+    nomCommissariat VARCHAR(50)
+) ENGINE = InnoB;
+
+--
+-- Table Services
+--
+DROP TABLE IF EXISTS Services;
 
 CREATE TABLE Services(
-   idService COUNTER,
-   nomService VARCHAR(50),
-   PRIMARY KEY(idService)
-);
+    idService INT AUTO_INCREMENT PRIMARY KEY,
+    nomService VARCHAR(50)
+) ENGINE = InnoB;
+
+--
+-- Table Policiers
+--
+DROP TABLE IF EXISTS Policiers;
 
 CREATE TABLE Policiers(
-   idPolicier COUNTER,
-   nomPolicier VARCHAR(50),
-   prenomPolicier VARCHAR(50) NOT NULL,
-   matriculePolicier VARCHAR(50) NOT NULL,
-   idGrade INT NOT NULL,
-   PRIMARY KEY(idPolicier),
-   UNIQUE(matriculePolicier),
-   FOREIGN KEY(idGrade) REFERENCES Grades(idGrade)
-);
+    idPolicier INT AUTO_INCREMENT PRIMARY KEY,
+    nomPolicier VARCHAR(50),
+    prenomPolicier VARCHAR(50) NOT NULL,
+    matriculePolicier VARCHAR(50) NOT NULL UNIQUE,
+    idGrade INT NOT NULL
+) ENGINE = InnoB;
 
-CREATE TABLE Héberge(
-   idVille INT,
-   idCommissariat INT,
-   PRIMARY KEY(idVille, idCommissariat),
-   FOREIGN KEY(idVille) REFERENCES Villes(idVille),
-   FOREIGN KEY(idCommissariat) REFERENCES Commissariats(idCommissariat)
-);
+--
+-- Table Localisation
+--
+DROP TABLE IF EXISTS Localisations;
 
-CREATE TABLE Affecté(
-   idCommissariat INT,
-   idService INT,
-   idPolicier INT,
-   dateAffectation DATE NOT NULL,
-   PRIMARY KEY(idCommissariat, idService, idPolicier),
-   FOREIGN KEY(idCommissariat) REFERENCES Commissariats(idCommissariat),
-   FOREIGN KEY(idService) REFERENCES Services(idService),
-   FOREIGN KEY(idPolicier) REFERENCES Policiers(idPolicier)
-);
+CREATE TABLE Localisations(
+    idLocalisation INT AUTO_INCREMENT PRIMARY KEY,
+    idVille INT,
+    idCommissariat INT
+) ENGINE = InnoB;
 
-CREATE TABLE Obtenu(
-   idBlame INT,
-   idPolicier INT,
-   dateBlame DATE,
-   PRIMARY KEY(idBlame, idPolicier),
-   FOREIGN KEY(idBlame) REFERENCES Blames(idBlame),
-   FOREIGN KEY(idPolicier) REFERENCES Policiers(idPolicier)
-);
+--
+-- Table Affectation
+--
+DROP TABLE IF EXISTS Affectations;
+
+CREATE TABLE Affectations(
+    idAffectation INT AUTO_INCREMENT PRIMARY KEY,
+    idCommissariat INT,
+    idService INT,
+    idPolicier INT,
+    dateAffectation DATE NOT NULL
+) ENGINE = InnoB;
+
+--
+-- Table Punition
+--
+DROP TABLE IF EXISTS Punitions;
+
+CREATE TABLE Punitions(
+    idPunition INT AUTO_INCREMENT PRIMARY KEY,
+    idBlame INT,
+    idPolicier INT,
+    dateBlame DATE
+) ENGINE = InnoB;
+
+--
+-- constraintes clés étrangères
+--
+ALTER TABLE
+    Policiers
+ADD
+    CONSTRAINT FK_Policiers_Grades FOREIGN KEY(idGrade) REFERENCES Grades(idGrade);
+
+ALTER TABLE
+    Localisations
+ADD
+    CONSTRAINT FK_Localisations_Villes FOREIGN KEY(idVille) REFERENCES Villes(idVille),
+ADD
+    CONSTRAINT FK_Localisations_Commissariats FOREIGN KEY(idCommissariat) REFERENCES Commissariats(idCommissariat);
+
+ALTER TABLE
+    Affectations
+ADD
+    CONSTRAINT FK_Affectations_Commissariats FOREIGN KEY(idCommissariat) REFERENCES Commissariats(idCommissariat),
+ADD
+    CONSTRAINT FK_Affectations_Services FOREIGN KEY(idService) REFERENCES Services(idService);
+
+ALTER TABLE
+    Affectations
+ADD
+    CONSTRAINT FK_Affectations_Policiers FOREIGN KEY(idPolicier) REFERENCES Policiers(idPolicier);
+
+ALTER TABLE
+    Punitions
+ADD
+    CONSTRAINT FK_Punitions_Policiers FOREIGN KEY(idPolicier) REFERENCES Policiers(idPolicier),
+ADD
+    CONSTRAINT FK_Punitions_Blames FOREIGN KEY(idBlame) REFERENCES Blames(idBlame);
+
+    
