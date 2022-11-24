dcf3ef682a5634dc9575e5bf1a34d24bf81fd79e
Neodart <neodart@gmail.com>
2022-11-24 12:56:38 +0100

    24/11/22 - 12h56 - Exo3 Finit

--- Exercices/01 - Persistance des données/03 - Développer des composants/Exercices/Exercice 3/Reponses.sql
+++ Exercices/01 - Persistance des données/03 - Développer des composants/Exercices/Exercice 3/Reponses.sql
@@ -42,7 +42,7 @@ WHERE
     av.note >= 12
     AND av.idEpreuve = 4;
 
--- D)Le nom des étudiants qui ont dans l'épreuve 4 une note supérieure à celle obtenue par « LUC DUPONT »(à cette même épreuve).
+-- D) Le nom des étudiants qui ont dans l'épreuve 4 une note supérieure à celle obtenue par « LUC DUPONT »(à cette même épreuve).
 SELECT
     CONCAT(et.nomEtudiant, " ", et.prenomEtudiant) AS Nom
 FROM
@@ -113,7 +113,7 @@ ALTER TABLE
 MODIFY
     prenomEtudiant varchar(20) NOT NULL;
 
--- I)Insérez les enregistrements suivants: 7, 'interro écrite',9,1,'21-oct-96',1 dans EPREUVE 
+-- I) Insérez les enregistrements suivants: 7, 'interro écrite',9,1,'21-oct-96',1 dans EPREUVE 
 INSERT INTO
     epreuves (
         idEpreuve,
@@ -208,7 +208,7 @@ DELETE FROM
 WHERE
     idEpreuve = 7;
 
--- M)Réflexion : N'y aurait-il pas eu moyen de détruire les notes de l'épreuve automatiquement dès la destruction de l'épreuve ?
+-- M) Réflexion : N'y aurait-il pas eu moyen de détruire les notes de l'épreuve automatiquement dès la destruction de l'épreuve ?
 -- Ajouter "ON DELETE CASCADE" pour la clé étrangère idEpreuve dans avoir_note
 -- passer de 
 --    ALTER TABLE `avoir_note`
@@ -224,12 +224,40 @@ ADD
 
 -- N) Changez toutes les notes de MARKE dans la matière « BASES DE DONNEES ». Suite à un mauvais comportement, elles diminuent toutes de 3 points. Attention, la requête doit intégrer le nom de la matière. (et non chercher à repérer le numéro avant de la taper.)
 UPDATE
-    avoir_note AS av    
+    avoir_note AS av
     INNER JOIN etudiants AS et ON av.idEtudiant = et.idEtudiant
     INNER JOIN epreuves AS ep ON av.idEpreuve = ep.idEpreuve
     INNER JOIN matieres as ma ON ep.idMatiereEpreuve = ma.idMatiere
 SET
     note = note - 3
-    WHERE et.nomEtudiant = "MARKE" 
+WHERE
+    et.nomEtudiant = "MARKE" -- pas de ligne « BASES DE DONNEES » dans matieres, seulement "BD"
     AND ma.nomMatiere = "BD";
-    -- pas de ligne « BASES DE DONNEES » dans matieres, seulement "BD"
+
+-- O) DEWA a manqué l'épreuve 4. Vu son niveau, on décide de lui créer une entrée dans AVOIR_NOTE en lui attribuant la moyenne des notes obtenues à cette épreuve par ses collègues*0.9. Attention, la requête doit intégrer le nom de l'étudiant (et non chercher à repérer le numéro avant de la taper.)
+INSERT INTO
+    avoir_note AS av (idEtudiant, idEpreuve, note)
+VALUES
+    (
+        (
+            SELECT
+                idEtudiant
+            FROM
+                etudiants
+            WHERE
+                nomEtudiant = "DEWA";
+
+),
+4,
+(
+    SELECT
+        AVG(note) * 0.9
+    FROM
+        avoir_note
+    WHERE
+        idEpreuve = 4
+)
+);
+
+-- P) Insérez un nouvel étudiant dont vous ne connaissez que le numéro, le nom, le prénom, le hobby et l'année: 25, 'DARTE','REMY','SCULPTURE',1.
+INSERT INTO etudiants (idEtudiant, nomEtudiant, prenomEtudiant, hobby, anneeEtudiant) VALUES (25,'DARTE','REMY','SCULPTURE',1);
