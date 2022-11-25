df1f39866064e4442f97d2f68fc5e05fd206f28c
Neodart <neodart@gmail.com>
2022-11-25 11:37:52 +0100

    25/11/22 - 11h37 - Fin matiné

--- Exercices/01 - Persistance des données/03 - Développer des composants/Exercices/Exercice 4/Partie2.sql
+++ Exercices/01 - Persistance des données/03 - Développer des composants/Exercices/Exercice 4/Partie2.sql
@@ -34,10 +34,10 @@ WHERE
 --
 -- 4 Rechercher le nom et le salaire des employés qui gagnent plus que leur patron, et le nom et le salaire de leur patron.
 SELECT
-    e.nom,
-    e.salaire,
-    p.nom,
-    p.salaire
+    e.nom AS "Nom Employé",
+    e.salaire AS "Salaire Employé",
+    p.nom AS "Nom du Patron",
+    p.salaire AS "Salaire du Patron"
 FROM
     employe AS e
     INNER JOIN employe AS p ON e.nosup = p.noemp
@@ -195,25 +195,79 @@ GROUP BY
     d.noregion;
 
 -- 3 Afficher les numéros des départements ayant au moins 3 employés.
-SELECT nodep FROM employe GROUP BY nodep HAVING COUNT(*)>=3;
+SELECT
+    nodep
+FROM
+    employe
+GROUP BY
+    nodep
+HAVING
+    COUNT(*) >= 3;
 
 -- 4 Afficher les lettres qui sont l'initiale d'au moins trois employés.
-SELECT LEFT(nom,1) FROM employe GROUP BY LEFT(nom,1) HAVING COUNT(*)>=3;
+SELECT
+    LEFT(nom, 1)
+FROM
+    employe
+GROUP BY
+    LEFT(nom, 1)
+HAVING
+    COUNT(*) >= 3;
 
 -- 5 Rechercher le salaire maximum et le salaire minimum parmi tous les salariés et l'écart entre les deux.
-SELECT MAX(salaire) AS "Salaire Max", MIN(salaire) AS "Salaire Min", MAX(salaire)-MIN(salaire) AS "Écart de salaire" FROM employe;
+SELECT
+    MAX(salaire) AS "Salaire Max",
+    MIN(salaire) AS "Salaire Min",
+    MAX(salaire) - MIN(salaire) AS "Écart de salaire"
+FROM
+    employe;
 
 -- 6 Rechercher le nombre de titres différents.
-SELECT COUNT(DISTINCT titre) AS "Nbre de titres" FROM employe;
+SELECT
+    COUNT(DISTINCT titre) AS "Nbre de titres"
+FROM
+    employe;
 
 -- 7 Pour chaque titre, compter le nombre d'employés possédant ce titre.
-SELECT titre, COUNT(*) AS "Effectif" FROM employe GROUP BY titre;
+SELECT
+    titre,
+    COUNT(*) AS "Effectif"
+FROM
+    employe
+GROUP BY
+    titre;
 
 -- 8 Pour chaque nom de département, afficher le nom du département et le nombre d'employés.
-SELECT d.nom AS "Département", COUNT(*) AS "Effectif" FROM dept AS d LEFT JOIN employe AS e ON d.nodept=e.nodep GROUP BY d.nom;
+SELECT
+    d.nom AS "Département",
+    COUNT(*) AS "Effectif"
+FROM
+    dept AS d
+    LEFT JOIN employe AS e ON d.nodept = e.nodep
+GROUP BY
+    d.nom;
 
 -- 9 Rechercher les titres et la moyenne des salaires par titre dont la moyenne est supérieure à la moyenne des salaires des Représentants.
-SELECT titre, ROUND(AVG(salaire),2) AS "Salaire moyen du titre" FROM employe GROUP BY titre HAVING AVG(salaire)>(SELECT AVG(salaire) FROM employe WHERE titre ="représentant");
+SELECT
+    titre,
+    ROUND(AVG(salaire), 2) AS "Salaire moyen du titre"
+FROM
+    employe
+GROUP BY
+    titre
+HAVING
+    AVG(salaire) >(
+        SELECT
+            AVG(salaire)
+        FROM
+            employe
+        WHERE
+            titre = "représentant"
+    );
 
 -- 10 Rechercher le nombre de salaires renseignés et le nombre de taux de commission renseignés.
-SELECT COUNT(salaire) AS "Nbre salaires renseignés", COUNT(tauxcom) AS "Nbre taux commissions renseignés" FROM employe;
+SELECT
+    COUNT(salaire) AS "Nbre salaires renseignés",
+    COUNT(tauxcom) AS "Nbre taux commissions renseignés"
+FROM
+    employe;
