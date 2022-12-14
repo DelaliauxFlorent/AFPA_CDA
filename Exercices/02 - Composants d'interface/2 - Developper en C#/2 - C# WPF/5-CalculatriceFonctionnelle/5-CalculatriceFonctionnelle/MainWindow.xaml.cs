cb4c04e7909b5e36f8c4b6a68ca434e653f65c8a
Florent <florent.delaliaux@gmail.com>
2022-12-14 17:10:07 +0100

    14/12/2022 17:10:06,02

--- Exercices/02 - Composants d'interface/2 - Developper en C#/2 - C# WPF/5-CalculatriceFonctionnelle/5-CalculatriceFonctionnelle/MainWindow.xaml.cs
+++ Exercices/02 - Composants d'interface/2 - Developper en C#/2 - C# WPF/5-CalculatriceFonctionnelle/5-CalculatriceFonctionnelle/MainWindow.xaml.cs
@@ -20,10 +20,10 @@ namespace _5_CalculatriceFonctionnelle
     /// </summary>
     public partial class MainWindow : Window
     {
-        private List<Double> memorisationNum = new List<Double>();
         private String memorisationSign;
         private Double resultat = 0;
-        private char[] listeOperandes = { '+', '-', '*', '/' };
+        private String[] listeOperandes = { "+", "-", "*", "/"};
+        private bool justEqual = false;
 
         public MainWindow()
         {
@@ -32,15 +32,37 @@ namespace _5_CalculatriceFonctionnelle
 
         private void num_Click(object sender, RoutedEventArgs e)
         {
+            if (justEqual)
+            {
+                txtblckAfficheurActu.Text = "";
+                txtblckAfficheurMemo.Text = "";
+                memorisationSign = null;
+                justEqual = false;
+            }
             txtblckAfficheurActu.Text += ((Button)(sender)).Content;
         }
 
         private void symb_Click(object sender, RoutedEventArgs e)
         {
+            //if (txtblckAfficheurActu.Text == "" &&
+            //    (txtblckAfficheurMemo.Text[txtblckAfficheurMemo.Text.Length-1]=='+' ||
+            //    txtblckAfficheurMemo.Text[txtblckAfficheurMemo.Text.Length - 1] == '-' ||
+            //    txtblckAfficheurMemo.Text[txtblckAfficheurMemo.Text.Length - 1] == '*' ||
+            //    txtblckAfficheurMemo.Text[txtblckAfficheurMemo.Text.Length - 1] == '/'))
+            //{
+            //    txtblckAfficheurMemo.Text = txtblckAfficheurMemo.Text.Remove(txtblckAfficheurMemo.Text.Length-1, 1);
+            //    txtblckAfficheurMemo.Text += ((Button)(sender)).Content;
+            //    memorisationSign= (String)((Button)(sender)).Content;
+            //}
             if (Double.TryParse(txtblckAfficheurActu.Text, out double valeurActu))
             {
+                if (justEqual)
+                {
+                    txtblckAfficheurMemo.Text = "";
+                    memorisationSign = null;
+                    justEqual = false;
+                }
                 txtblckAfficheurMemo.Text += txtblckAfficheurActu.Text + ((Button)(sender)).Content;
-                memorisationNum.Add(valeurActu);
                 if (memorisationSign != null)
                 {
                     switch (memorisationSign)
@@ -66,9 +88,11 @@ namespace _5_CalculatriceFonctionnelle
                     }
                     if ((String)((Button)(sender)).Content == "=")
                     {
-                        txtblckAfficheurMemo.Text = "";
-                        txtblckAfficheurActu.Text = resultat.ToString();
+                        //txtblckAfficheurMemo.Text = "";
+                        //txtblckAfficheurActu.Text = resultat.ToString();
+                        txtblckAfficheurActu.Text = eval(txtblckAfficheurMemo.Text.Remove(txtblckAfficheurMemo.Text.Length-1)).ToString();
                         memorisationSign = null;
+                        justEqual = true;
                     }
                 }
                 else
@@ -78,14 +102,12 @@ namespace _5_CalculatriceFonctionnelle
                 }
                 memorisationSign = (String)((Button)(sender)).Content;
             }
-
         }
 
         private void cancel_Click(object sender, RoutedEventArgs e)
         {
             memorisationSign = null;
             resultat = 0;
-            memorisationNum.Clear();
             txtblckAfficheurMemo.Text = "";
             cancelCur_Click(sender, e);
         }
@@ -96,14 +118,70 @@ namespace _5_CalculatriceFonctionnelle
 
         private Double eval(String histo)
         {
-            if(Double.TryParse(histo, out Double valeur))
+            if (Double.TryParse(histo, out Double valeur))
             {
                 return valeur;
             }
             else
             {
-
+                int indexListeCar = 0;
+                String[] tableau;
+                String sign = " ";
+                do
+                {
+                    String[] caractSplit = { listeOperandes[indexListeCar] };
+                    tableau = histo.Split(caractSplit, 2);
+                    histo.Split()
+                    if (tableau.Length == 2)
+                    {
+                        sign = listeOperandes[indexListeCar];
+                    }
+                    else
+                    {
+                        indexListeCar++;
+                    }
+                } while ((indexListeCar < listeOperandes.Length) && !listeOperandes.Contains(sign));
+                if (listeOperandes.Contains(tableau[0][tableau[0].Length - 1]))
+                {
+                    tableau[1] = sign + tableau[1];
+                    sign = tableau[0][tableau[0].Length - 1];
+                    tableau[0] = tableau[0].Remove(tableau[0].Length - 1);
+                }
+                switch (sign)
+                {
+                    case '+':
+                        return eval(tableau[0]) + eval(tableau[1]);
+                    case '-':
+                        return eval(tableau[0]) - eval(tableau[1]);
+                    case '*':
+                        return eval(tableau[0]) * eval(tableau[1]);
+                    case '/':
+                        return eval(tableau[0]) / eval(tableau[1]);
+                    default:
+                        return 0;
+                }
             }
+        }
+
+        private void pM_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtblckAfficheurActu.Text[0] == '-')
+            {
+                txtblckAfficheurActu.Text = txtblckAfficheurActu.Text.Remove(0, 1);
+            }
+            else
+            {
+                txtblckAfficheurActu.Text = "-" + txtblckAfficheurActu.Text;
+            }
+        }
+
+        private void delet_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtblckAfficheurActu.Text.Length > 0)
+            {
+                txtblckAfficheurActu.Text = txtblckAfficheurActu.Text.Remove(txtblckAfficheurActu.Text.Length - 1, 1);
+            }
+
         }
     }
 }
