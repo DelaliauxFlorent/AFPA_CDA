1deec8165044b02c1aa402df550f2b038b39279f
Florent <florent.delaliaux@gmail.com>
2022-12-21 12:10:06 +0100

    21/12/2022 12:10:05,10

--- Exercices/02 - Composants d'interface/3 - Accès aux données/1 - Cours/3-CreerAPI/3-CreerAPI/Controllers/PersonnesController.cs
+++ Exercices/02 - Composants d'interface/3 - Accès aux données/1 - Cours/3-CreerAPI/3-CreerAPI/Controllers/PersonnesController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using _3_CreerAPI.Data.Dtos;
+using _3_CreerAPI.Data.Services;
+using _3_CreerAPI.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,8 +10,47 @@ using System.Threading.Tasks;
 
 namespace _3_CreerAPI.Controllers
 {
+    [Route("api/Personnes")]
+    [ApiController]
     public class PersonnesController:ControllerBase
     {
+        private readonly PersonnesServices _service;
+        private readonly IMapper _mapper;
 
+        public PersonnesController(PersonnesServices service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        //GET api/personnes
+        [HttpGet]
+        public ActionResult<IEnumerable<PersonnesDTO>> getAllPersonnes()
+        {
+            var listePersonnes = _service.GetAllPersonnes();
+            return Ok(_mapper.Map<IEnumerable<PersonnesDTO>>(listePersonnes));
+        }
+
+        //Get api/personnes/{id}
+        [HttpGet("{id}", Name ="GetPersonneById")]
+        public ActionResult<PersonnesDTO> GetPersonneById(int id)
+        {
+            var commandItem = _service.GetPersonneById(id);
+            if (commandItem != null)
+            {
+                return Ok(_mapper.Map<PersonnesDTO>(commandItem));
+            }
+            return NotFound();
+        }
+
+        //POST api/personnes
+        [HttpPost]
+        public ActionResult<PersonnesDTO> CreatePersonne(Personne personne)
+        {
+            //on ajoute l'objet à la base de données
+            _service.AddPersonnes(personne);
+            //on retourne le chemin de FindById avec l'objet créé
+            return CreatedAtRoute(nameof(GetP))
+        }
     }
 }
