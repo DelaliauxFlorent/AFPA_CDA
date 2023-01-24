ac2f8185aafb550290f990cae670950858c13ca8
FlorentDelaliaux <florent.delaliaux@gmail.com>
2023-01-24 17:10:04 +0100

    24/01/2023 17:10:03,72

--- Exercices/03 - Appli Web/01 - 1er Essai/PHP/MODEL/MANAGER/PersonneManager.Class.php
+++ Exercices/03 - Appli Web/01 - 1er Essai/PHP/MODEL/MANAGER/PersonneManager.Class.php
@@ -13,10 +13,15 @@ class PersonneManager{
     #endregion Accesseurs
     ////////////////////////////////////
 
+    /**
+     * Récupérer toutes les personnes
+     *
+     * @return void
+     */
     public static function GetAllPersonnes()
     {
         $stmt = DbConnect::getDb()->prepare("
-        SELECT * FROM Personnes
+        SELECT * FROM personnes
         ");
         $stmt->execute();
         $result=$stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -26,14 +31,19 @@ class PersonneManager{
         return $ListPersonnes;
     }
 
+    /**
+     * Récupérer une personne précise
+     *
+     * @param integer $idVoulu
+     * @return void
+     */
     public static function GetPersonneById(int $idVoulu)
     {
         $stmt = DbConnect::getDb()->prepare("
-        SELECT * FROM Personnes WHERE id=:idVoulu
+        SELECT * FROM personnes WHERE id=:idVoulu
         ");
-        $stmt->execute([
-            ":idVoulu"=>$idVoulu
-        ]);
+        $stmt->bindValue(':idVoulu', $idVoulu, PDO::PARAM_INT);
+        $stmt->execute();
         $individu=new Personne($stmt->fetch(PDO::FETCH_ASSOC));
         return $individu;
     }
@@ -48,17 +58,48 @@ class PersonneManager{
      * @param [string] $ville
      * @return void
      */
-    public static function AddPersonne($nom = null, $prenom =null, $codePostal =null, $adresse, $ville)
+    public static function AddPersonne(string $nom = null, string $prenom =null, int $codePostal =null, string $adresse, string $ville)
     {
         $stmt = DbConnect::getDb()->prepare("
-        INSERT INTO PERSONNES VALUES (null, :nom, :prenom, :codePostal, :adresse, :ville)
+        INSERT INTO personnes VALUES (null, :nom, :prenom, :codePostal, :adresse, :ville)
         ");
-        $stmt->execute([
-            ":nom"=>$nom,
-            ":prenom"=>$prenom,
-            ":codePostal"=>$codePostal,
-            ":adresse"=>$adresse,
-            ":ville"=>$ville
-        ]);
+        $stmt->bindValue(":nom",$nom,PDO::PARAM_STR);
+        $stmt->bindValue(":prenom",$prenom,PDO::PARAM_STR);
+        $stmt->bindValue(":codePostal",$codePostal,PDO::PARAM_INT);
+        $stmt->bindValue(":adresse",$adresse,PDO::PARAM_STR);
+        $stmt->bindValue(":ville",$ville,PDO::PARAM_STR);
+        $stmt->execute();
     }
+
+    public static function UpdatePersonne(int $id, string $nom = null, string $prenom=null, int $codePostal=null, string $adresse = null, string $ville=null)
+    {
+        $champs=["nom", "prenom", "codePostal", "adresse", "ville"];
+        $params=[$nom, $prenom, $codePostal, $adresse, $ville];
+        $liste=[];
+        $sql = "UPDATE personnes SET ";
+        $modif=0;
+        for ($i=0; $i < count($params); $i++) { 
+            if($params[$i]!=null){
+                $sql.=$champs[$i]."=:".$champs[$i].",";
+                $liste[]=$i;
+                $modif++;
+            }
+        }
+        if($modif!=0){
+            $sql=substr($sql, 0,-1)." WHERE id = :id";
+            var_dump($sql);
+            $stmt = DbConnect::getDb()->prepare($sql);
+            for ($i=0; $i < count($liste); $i++) { 
+                $stmt->bindValue(":".$champ[$liste[$i]], $params[$liste[$i]], )
+            }
+            $stmt->bindValue(":id", $id, PDO::PARAM_INT);
+            //$stmt->bindValue(":nom", $nom, PDO::PARAM_STR);
+            $stmt->bindValue(":prenom", $prenom, PDO::PARAM_STR);
+            //$stmt->bindValue(":codePostal", $codePostal, PDO::PARAM_INT);
+            $stmt->bindValue(":adresse", $adresse, PDO::PARAM_STR);
+            $stmt->bindValue(":ville", $ville, PDO::PARAM_STR);
+            return $stmt->execute();            
+        }
+    }
+
 }
